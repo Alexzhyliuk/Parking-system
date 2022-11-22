e6bac0242919b7f87948dfe537076099c87fb8cd
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2022-11-22 14:01:39 +0300

    front update hover parking places

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
+from random import randint
 
 
 def index(request):
+
     return render(
         request,
-        "main/index.html"
+        "main/index.html",
+        {
+            "places": {i: "booked" if randint(0, 1) else "" for i in range(1, 26)},
+        }
     )
